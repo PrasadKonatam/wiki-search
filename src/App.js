6247e72a19c28bd83474d364442407c6cad26391
dpkonatam <dpkonatam@krenovate.com>
2024-07-02 12:04:03 +0530

    new commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+  const [searchInput, setSearchInput] = useState("");
+  const [searchResults, setSearchResults] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const searchWikipedia = async (event) => {
+    if (event.key === "Enter") {
+      setIsLoading(true);
+      setSearchResults([]);
+
+      const url = `https://apis.ccbp.in/wiki-search?search=${searchInput}`;
+      const response = await fetch(url);
+      const jsonData = await response.json();
+      const { search_results } = jsonData;
+      setSearchResults(search_results);
+      setIsLoading(false);
+    }
+  };
+
+  const createAndAppendSearchResult = (result) => {
+    const { link, title, description } = result;
+
+    return (
+      <div className="result-item" key={link}>
         <a
-          className="App-link"
-          href="https://reactjs.org"
+          href={link}
           target="_blank"
           rel="noopener noreferrer"
+          className="result-title"
         >
-          Learn React
+          {title}
         </a>
-      </header>
+        <br />
+        <a
+          href={link}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="result-url"
+        >
+          {link}
+        </a>
+        <br />
+        <p className="link-description">{description}</p>
+      </div>
+    );
+  };
+
+  return (
+    <div className="main-container">
+      <div className="wiki-search-header">
+        <img
+          className="wiki-logo"
+          src="https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/wiki-logo-img.png"
+          alt="Wiki Logo"
+        />
+        <input
+          type="search"
+          className="search-input"
+          placeholder="Type a keyword and press Enter to search"
+          value={searchInput}
+          onChange={(e) => setSearchInput(e.target.value)}
+          onKeyDown={searchWikipedia}
+        />
+      </div>
+      {isLoading && (
+        <div className="spinner">
+          <div className="spinner-border" role="status">
+            <span className="sr-only">Loading...</span>
+          </div>
+        </div>
+      )}
+      <div className="search-results">
+        {searchResults.map(createAndAppendSearchResult)}
+      </div>
     </div>
   );
 }
